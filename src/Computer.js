0884646219386a98d026e920b17c702da85e4e54
ANURAG <1928081@kiit.ac.in>
2022-05-30 19:36:55 +0530

    science comp

--- src/Computer.js
+++ src/Computer.js
@@ -1,10 +1,130 @@
 import React from 'react'
-
+import { useState } from "react";
 export default function Computer() {
+
+
+
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const questions = [
+    {
+      text: "Which of the following correctly declares an array in C++?",
+      options: [
+        { id: 0, text: "array{10};", isCorrect: false },
+        { id: 1, text: "array array[10];", isCorrect: false },
+        { id: 2, text: "int array;", isCorrect: false },
+        { id: 3, text: "int array[10];", isCorrect: true },
+      ],
+    },
+    {
+      text: " Pick the incorrect statement about inline functions in C++?",
+      options: [
+        { id: 0, text: "Saves overhead of a return call from a function", isCorrect: false },
+        { id: 1, text: "They are generally very large and complicated function", isCorrect: true },
+        { id: 2, text: "These functions are inserted/substituted at the point of call", isCorrect: false },
+        { id: 3, text: "They reduce function call overheads", isCorrect: false },
+      ],
+    },
+    {
+      text: " Which of the following constructors are provided by the C++ compiler if not defined in a class?",
+      options: [
+        { id: 0, text: "Copy constructor", isCorrect: false },
+        { id: 1, text: "Default constructor", isCorrect: false },
+        { id: 2, text: "Assignment constructor", isCorrect: false },
+        { id: 3, text: "All of the mentioned", isCorrect: true },
+      ],
+    },
+    {
+      text: "What is the benefit of c++ input and output over c input and output?",
+      options: [
+        { id: 0, text: "Both Type safety & Exception", isCorrect: false },
+        { id: 1, text: "Sequence container", isCorrect: false },
+        { id: 2, text: "Exception", isCorrect: false },
+        { id: 3, text: "Type safety", isCorrect: true },
+      ],
+    },
+    {
+      text: " What is the meaning of the following declaration:int(*p[5])(); ? ",
+      options: [
+        { id: 0, text: "p is pointer to function", isCorrect: false },
+        { id: 1, text: "p is array of pointer to function", isCorrect: true },
+        { id: 2, text: "p is pointer to such function which return type is the array", isCorrect: false },
+        { id: 3, text: "p is pointer to array of function", isCorrect: false },
+      ],
+    },
+  ];
+
+  // Helper Functions
+
+  /* A possible answer was clicked */
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      //question length completed
+      setShowResults(true);
+    }
+  };
+
+  /* Resets the game back to default */
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
   return (
     <div>
+      <div className="App">
+        {/* 1. Header  */}
+        <h1>USA Quiz 🇺🇸</h1>
 
-        
+        {/* 2. Current Score  */}
+        <h2>Score: {score}</h2>
+
+        {/* 3. Show results if question length completes*/}
+        {showResults ? (
+          /* 4. Final Results */
+          <div className="final-results">
+            <h1>Final Results</h1>
+            <h2>
+              {score} out of {questions.length} correct - (
+              {(score / questions.length) * 100}%)
+            </h2>
+            <button onClick={() => restartGame()}>Restart game</button>
+          </div>
+        ) : (
+          /* 5.else show Question Card  */
+          <div className="question-card">
+            {/* Current Question  */}
+            <h2>
+              Question: {currentQuestion + 1} out of {questions.length}
+            </h2>
+            <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+            {/* List of possible answers  */}
+            <ul>
+              {questions[currentQuestion].options.map((option) => {
+                return (
+                  <li
+                    key={option.id}
+                    onClick={() => optionClicked(option.isCorrect)}
+                  >
+                    {option.text}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+
     </div>
   )
 }
--- src/Science.js
+++ src/Science.js
@@ -1,10 +1,130 @@
 import React from 'react'
-
+import { useState } from "react";
 export default function Science() {
+
+
+
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const questions = [
+    {
+      text: "The temperature coefficient of resistance of a wire is 0.00125 per °C. At 300 K, its resistance is 1 ohm. This resistance of the wire will be 2 ohm at.",
+      options: [
+        { id: 0, text: "1154K", isCorrect: false },
+        { id: 1, text: "1100K", isCorrect: false },
+        { id: 2, text: "1400K", isCorrect: false },
+        { id: 3, text: "1127K", isCorrect: true },
+      ],
+    },
+    {
+      text: "A current 4.0 A exist in a wire of cross-sectional area 2.0 mm2.If each cubic metre of the wire contains 12.0 × 10^^28 free electrons then the drift speed is",
+      options: [
+        { id: 0, text: "2x10^-8 m/s", isCorrect: false },
+        { id: 1, text: "0.5x10^-3 m/s", isCorrect: false },
+        { id: 2, text: "1.04x10^-4 m/s", isCorrect: true },
+        { id: 3, text: "None of These", isCorrect: false },
+      ],
+    },
+    {
+      text: "A metallic oxide reacts with water to form its hydroxide, hydrogen peroxide and also liberates oxygen. The metallic oxide could be",
+      options: [
+        { id: 0, text: "CaO", isCorrect: false },
+        { id: 1, text: "KO2", isCorrect: true },
+        { id: 2, text: "Li2O", isCorrect: false },
+        { id: 3, text: "Na2O2", isCorrect: false },
+      ],
+    },
+    {
+      text: "Lithium shows diagonal relationship with",
+      options: [
+        { id: 0, text: "Sodium", isCorrect: false },
+        { id: 1, text: "Magnesium", isCorrect: true },
+        { id: 2, text: "Calcium", isCorrect: false },
+        { id: 3, text: "Aluminium", isCorrect: false },
+      ],
+    },
+    {
+      text: "Aluminium hydroxide, Al(OH)3 is insoluble in water, but dissolves readily in both acidic and basic solutions. Such behaviour is characteristic of",
+      options: [
+        { id: 0, text: "polyprotic behaviour", isCorrect: false },
+        { id: 1, text: "hydrophyllic behaviour", isCorrect: false },
+        { id: 2, text: "a buffer", isCorrect: false },
+        { id: 3, text: "amphoteric behaviour", isCorrect: true },
+      ],
+    },
+  ];
+
+  // Helper Functions
+
+  /* A possible answer was clicked */
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      //question length completed
+      setShowResults(true);
+    }
+  };
+
+  /* Resets the game back to default */
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
   return (
     <div>
+      <div className="App">
+        {/* 1. Header  */}
+        <h1>USA Quiz 🇺🇸</h1>
 
-        
+        {/* 2. Current Score  */}
+        <h2>Score: {score}</h2>
+
+        {/* 3. Show results if question length completes*/}
+        {showResults ? (
+          /* 4. Final Results */
+          <div className="final-results">
+            <h1>Final Results</h1>
+            <h2>
+              {score} out of {questions.length} correct - (
+              {(score / questions.length) * 100}%)
+            </h2>
+            <button onClick={() => restartGame()}>Restart game</button>
+          </div>
+        ) : (
+          /* 5.else show Question Card  */
+          <div className="question-card">
+            {/* Current Question  */}
+            <h2>
+              Question: {currentQuestion + 1} out of {questions.length}
+            </h2>
+            <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+            {/* List of possible answers  */}
+            <ul>
+              {questions[currentQuestion].options.map((option) => {
+                return (
+                  <li
+                    key={option.id}
+                    onClick={() => optionClicked(option.isCorrect)}
+                  >
+                    {option.text}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+
     </div>
   )
 }
